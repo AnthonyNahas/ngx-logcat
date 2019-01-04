20db826ffd363090a18b17af80fa2f0361fd7d02
Anthony Nahas <anthony.na@hotmail.de>
2019-01-04 12:19:32 +0200

    demo(app): added an log example via logcat to the home component

--- demo/src/app/home/home.component.ts
+++ demo/src/app/home/home.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import { Title }     from '@angular/platform-browser';
+import {Component, OnInit} from '@angular/core';
+import {Title} from '@angular/platform-browser';
 import sdk from '@stackblitz/sdk';
+import {Logcat} from 'ngx-logcat';
 
 @Component({
   selector: 'app-home',
@@ -9,13 +10,22 @@ import sdk from '@stackblitz/sdk';
 })
 export class HomeComponent implements OnInit {
 
-  constructor(private titleService:Title) { }
+  constructor(private titleService: Title,
+              public logcat: Logcat) {
+  }
 
   ngOnInit() {
     this.titleService.setTitle('Home | ngx-logcat');
+    this.logcat
+      .t('HOME_COMPONENT')
+      .log('HomeComponent has been just initialized');
+    this.logcat
+      .t('DEBUGGING')
+      .d('This is another debug log', 123, 'are my favorite numbers');
   }
 
   editOnStackBlitz() {
     sdk.openGithubProject('AnthonyNahas/ngx-logcat/tree/master/demo');
   }
+
 }
