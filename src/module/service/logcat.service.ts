6e677d35512f393104c9605fc95e0776c20af7fe
Anthony Nahas <anthony.na@hotmail.de>
2019-01-04 11:59:37 +0200

    feat(package): process auto table detection added

--- src/module/service/logcat.service.ts
+++ src/module/service/logcat.service.ts
@@ -24,7 +24,7 @@ export class Logcat {
 
   assert(message: boolean, ...logs: any[]): Logcat {
     if (this.canLog(Level.ASSERT)) {
-      console.debug(message, ...logs);
+      console.assert(message, ...logs);
     }
     return this;
   }
@@ -36,53 +36,114 @@ export class Logcat {
 
   debug(message: any, ...logs: any[]): Logcat {
     if (this.canLog(Level.DEBUG)) {
-      const timeStamp = new Date();
-      console.group(this._tag);
-      console.debug('message: ', message);
-      console.debug('logs: ', ...logs);
-      console.debug('timeStamp: ', timeStamp);
-      console.groupEnd();
+      this.processLog(Level.DEBUG, message, ...logs);
     }
     return this;
   }
 
   log(message: any, ...logs: any[]): Logcat {
-    if (this.config.enable) {
-      console.log(message, ...logs);
+    if (this.canLog(Level.DEBUG)) {
+      this.processLog(Level.LOG, message, ...logs);
     }
     return this;
   }
 
-  i(): Logcat {
-    return this.info(null);
+  i(message: any, ...logs: any[]): Logcat {
+    return this.info(message, ...logs);
   }
 
   info(message: any, ...logs: any[]): Logcat {
     if (this.config.enable) {
-      console.log(message, ...logs);
+      this.processLog(Level.INFO, message, ...logs);
     }
     return this;
   }
 
-  w() {
-
+  w(message: any, ...logs: any[]) {
+    return this.warn(message, ...logs);
   }
 
   warn(message: any, ...logs: any[]) {
-    console.warn(message, ...logs);
+    if (this.config.enable) {
+      this.processLog(Level.WARN, message, ...logs);
+    }
+    return this;
   }
 
-  e() {
-
+  e(message: any, ...logs: any[]) {
+    return this.error(message, ...logs);
   }
 
-  error(error: Error) {
-    // this.errorHandler.handleError(error);
+
+  error(message: any, ...logs: any[]) {
+    if (this.config.enable) {
+      this.processLog(Level.ERROR, message, ...logs);
+    }
+    return this;
   }
 
   clear(): Logcat {
     console.clear();
     return this;
+  }
+
+  processLog(level: Level, message: any, ...logs: any[]) {
+    const timeStamp = new Date();
+    console.group(this._tag);
+
+    switch (level) {
+      case Level.DEBUG:
+        this.processTablesWhenDetected(Level.DEBUG, message);
+        console.debug('logs: ', ...logs);
+        console.debug('timeStamp: ', timeStamp);
+        break;
+      case Level.LOG:
+        this.processTablesWhenDetected(Level.LOG, message);
+        console.log('logs: ', ...logs);
+        console.log('timeStamp: ', timeStamp);
+        break;
+      case Level.INFO:
+        console.info('message: ', message);
+        console.info('logs: ', ...logs);
+        console.info('timeStamp: ', timeStamp);
+        break;
+      case Level.WARN:
+        console.warn('message: ', message);
+        console.warn('logs: ', ...logs);
+        console.warn('timeStamp: ', timeStamp);
+        break;
+      case Level.ERROR:
+        console.error('message: ', message);
+        console.error('logs: ', ...logs);
+        console.error('timeStamp: ', timeStamp);
+        break;
+    }
+    console.groupEnd();
+  }
+
+  processTablesWhenDetected(level: Level, message: any): void {
+    if (this.autoDetectTables(message)) {
+      switch (level) {
+        case Level.DEBUG:
+          console.debug('message: ', 'table has been detected');
+          console.table(message);
+          break;
+
+        case Level.LOG:
+          console.log('message: ', 'table has been detected');
+          console.table(message);
+          break;
+      }
+    } else {
+      switch (level) {
+        case Level.DEBUG:
+          console.debug('message: ', message);
+          break;
+        case Level.LOG:
+          console.log('message: ', message);
+          break;
+      }
+    }
   }
 
   autoDetectTables(value: any): boolean {
@@ -94,4 +155,6 @@ export class Logcat {
     }
     return false;
   }
+
+
 }
