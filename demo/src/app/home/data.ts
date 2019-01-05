d56d48be779cccd6a316a56290ea9c2f1bd42e88
Anthony Nahas <anthony.na@hotmail.de>
2019-01-05 17:21:38 +0200

    demo(app): added examples into the home component

---
+++ demo/src/app/home/data.ts
@@ -0,0 +1,76 @@
+export const MockData =
+  [
+    {
+      'id': 1,
+      'first_name': 'Arthur',
+      'last_name': 'Poulgreen',
+      'email': 'apoulgreen0@nature.com',
+      'gender': 'Male',
+      'ip_address': '159.27.170.216'
+    },
+    {
+      'id': 2,
+      'first_name': 'Rupert',
+      'last_name': 'Forth',
+      'email': 'rforth1@unesco.org',
+      'gender': 'Male',
+      'ip_address': '167.155.208.41'
+    },
+    {
+      'id': 3,
+      'first_name': 'Cleve',
+      'last_name': 'Braunton',
+      'email': 'cbraunton2@sohu.com',
+      'gender': 'Male',
+      'ip_address': '3.113.124.120'
+    },
+    {
+      'id': 4,
+      'first_name': 'Andee',
+      'last_name': 'Beisley',
+      'email': 'abeisley3@msn.com',
+      'gender': 'Female',
+      'ip_address': '157.11.105.246'
+    },
+    {
+      'id': 5,
+      'first_name': 'Lucien',
+      'last_name': 'Spivie',
+      'email': 'lspivie4@nymag.com',
+      'gender': 'Male',
+      'ip_address': '145.64.90.170'
+    },
+    {
+      'id': 6,
+      'first_name': 'Allissa',
+      'last_name': 'Oakshott',
+      'email': 'aoakshott5@surveymonkey.com',
+      'gender': 'Female',
+      'ip_address': '233.53.14.183'
+    },
+    {'id': 7, 'first_name': 'Patrice', 'last_name': 'Pues', 'email': 'ppues6@blogger.com', 'gender': 'Male', 'ip_address': '98.160.206.85'},
+    {
+      'id': 8,
+      'first_name': 'Yovonnda',
+      'last_name': 'Agutter',
+      'email': 'yagutter7@howstuffworks.com',
+      'gender': 'Female',
+      'ip_address': '212.35.72.213'
+    },
+    {
+      'id': 9,
+      'first_name': 'Carla',
+      'last_name': 'Yakov',
+      'email': 'cyakov8@amazonaws.com',
+      'gender': 'Female',
+      'ip_address': '231.231.105.41'
+    },
+    {
+      'id': 10,
+      'first_name': 'Aubrey',
+      'last_name': 'Bloggett',
+      'email': 'abloggett9@163.com',
+      'gender': 'Male',
+      'ip_address': '212.18.70.155'
+    }
+  ];
--- demo/src/app/home/home.component.ts
+++ demo/src/app/home/home.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {Title} from '@angular/platform-browser';
 import sdk from '@stackblitz/sdk';
 import {Logcat} from 'ngx-logcat';
+import {MockData} from './data';
 
 @Component({
   selector: 'app-home',
@@ -10,18 +11,41 @@ import {Logcat} from 'ngx-logcat';
 })
 export class HomeComponent implements OnInit {
 
+  data = MockData;
+
   constructor(private titleService: Title,
               public logcat: Logcat) {
   }
 
   ngOnInit() {
     this.titleService.setTitle('Home | ngx-logcat');
+
+    this.logcat
+      .t('TABLE - Auto detection')
+      .d(this.data);
+
     this.logcat
       .t('HOME_COMPONENT')
       .log('HomeComponent has been just initialized');
+
+    this.logcat
+      .t('INFO_LOG')
+      .i('Here is a info log');
+
+    this.logcat
+      .t('WARN_LOG')
+      .w('Here is a warning log');
+
     this.logcat
       .t('DEBUGGING')
       .d('This is another debug log', 123, 'are my favorite numbers');
+
+
+    const error = new Error('Error occurred');
+
+    this.logcat
+      .t('ERROR - StackTrace')
+      .e(error, error.stack);
   }
 
   editOnStackBlitz() {
